bd289aadea17439361df2d006386459a9d8dbe7f
shyam.sandilya26005@gmail.com <shyam.sandilya26005@gmail.com>
2024-12-10 23:20:25 +0530

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['tailwind.config.js'], // Exclude Tailwind config
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-})
+});
